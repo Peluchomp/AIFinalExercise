fb8edf45eeef5d212db79679b8309bb329ddcf80
Peluchomp <pau.mora.sole@gmail.com>
2025-01-09 15:51:56 +0100

    Navmesh fix

---
+++ AIFinalExercise/Assets/WanderScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderScript : MonoBehaviour
+{
+    public float wanderRadius = 10f;
+    public float wanderTimer = 5f;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
